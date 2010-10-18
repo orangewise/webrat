8957351bdc1a0e75e5c78b0225c24de93047835c
Ronald Luitwieler <ronald.luitwieler@gmail.com>
2010-10-18 10:19:02 +0200

    trying to get rails3 and webrat running

--- lib/webrat/core/session.rb
+++ lib/webrat/core/session.rb
@@ -286,11 +286,13 @@ For example:
     end
 
     def current_host
-      URI.parse(current_url).host || @custom_headers["Host"] || "www.example.com"
+      # URI.parse(current_url).host || @custom_headers["Host"] || "www.example.com"
+      URI.parse(current_url).host || @custom_headers["Host"] || default_current_host 
     end
 
     def response_location_host
-      URI.parse(response_location).host || "www.example.com"
+      # URI.parse(response_location).host || "www.example.com"
+      adapter.class==Webrat::RackAdapter ? "example.org" : "www.example.com"
     end
 
     def reset
